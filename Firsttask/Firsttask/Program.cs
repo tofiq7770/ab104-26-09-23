49cc739afe1c4054a314ca3f02e5b12dd676abea
tofiq7770 <tofigtn@code.edu.az>
2023-09-27 03:38:52 +0400

    Adding Files

---
+++ Firsttask/Firsttask/Program.cs
@@ -0,0 +1,76 @@
+﻿
+//task1
+//int a = 7624;
+
+//int b = a % 10;
+//int c = (a % 100) / 10;
+//int d = (a % 1000) / 100;
+//int e = a / 1000;
+//int f = b + c + d + e;
+
+//Console.WriteLine(b);
+//Console.WriteLine(c);
+//Console.WriteLine(d);
+//Console.WriteLine(e);
+//Console.WriteLine(f);
+
+
+
+//task2
+
+//int a = 220;
+
+//int b = a % 10;
+//int c = (a % 100) / 10;
+//int d = (a / 100);
+
+//if (b == c && b == d && c == d)
+//{
+//    Console.WriteLine("Hamisi beraberdir");
+//}
+//else
+//{
+//    Console.WriteLine("ferqli reqemler var");
+
+//}
+
+
+
+//task3
+
+//int a = 5;
+//int b = 6;
+//int c = a;
+//a = b;
+//b = c;
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+
+
+
+//task4
+
+//Console.Write("Enter yout age-");
+//int age = int.Parse(Console.ReadLine());
+
+//double ticket;
+
+//Console.Write("Do youhave your Id-");
+//bool stuId= bool.Parse(Console.ReadLine()); 
+
+//if (age <= 12 || age >= 60)
+//{
+//    ticket = 5;
+//    Console.WriteLine($"{ticket}");
+//} else if (stuId==false) 
+//{
+//    ticket = 10;
+//    Console.WriteLine($"{ticket}");
+//}
+//else
+//{
+//    ticket = 7.5;
+//    Console.WriteLine($"{ticket}");
+//}
+
